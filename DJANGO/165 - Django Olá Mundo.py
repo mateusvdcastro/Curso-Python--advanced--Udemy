1754697725e02c2149463a5d4cb09a51116560ac
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-07-03 14:12:51 -0300

    Django secound

--- DJANGO/165 - Django Olá Mundo.py
+++ DJANGO/165 - Django Olá Mundo.py
@@ -2,5 +2,5 @@
 # no seu back-end
 # Digite no terminal para criar um projeto: django-admin startproject DJANGO .
 # Para abrir um server local: python manage.py runserver
-
+# Criar um app: python manage.py startapp blog
 
---
+++ DJANGO/sobre/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urspatterns = [
+    path('teste/', views.teste)
+]
