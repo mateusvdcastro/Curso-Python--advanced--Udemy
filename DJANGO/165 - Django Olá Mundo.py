612674881b52f9c5e8d2e0e984509b25fd4ea2b3
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-07-03 13:11:08 -0300

    Django init

---
+++ DJANGO/165 - Django Olá Mundo.py
@@ -0,0 +1,6 @@
+# Youtube, Instagram, Mozilla, FireFox utilizam Django
+# no seu back-end
+# Digite no terminal para criar um projeto: django-admin startproject DJANGO .
+# Para abrir um server local: python manage.py runserver
+
+
