c0f6e30b2c6e2db94783f61d573484e0f95c7156
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-09-21 17:18:56 -0300

    Update Django Agenda Project

--- DJANGO/agenda/contatos/urls.py
+++ DJANGO/agenda/contatos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('<int:contato_id>', views.ver_contato, name='ver_contato'),
 ]
